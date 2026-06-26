13f668af080eaa607e0f5807bb2ef0d5f7ca35e4
rlavinsh <rlavinsh@gmail.com>
2026-06-27 00:01:54 +0530

    functions questions updated

--- 1_Functions/app.js
+++ 1_Functions/app.js
@@ -1,5 +1,5 @@
 // Functions
-// 1. Function Declration 
+// 1. Function Declration
 // 2 Function expression
 // 3.Arrow Function
 //4.IIFE(Immediate Invoked function expression)
@@ -7,6 +7,44 @@
 //Invoke kar sakte ho
 
 // Syntax
- function FunctionName(){
-  
- }
+//  function FunctionName(){
+
+//  }\
+
+// function greet() {
+//   console.log("Hello Students");
+// }
+// greet();
+
+// with Parameters
+// function calculateSum(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// calculateSum(10, 15);
+// console.log("==========Output of 2 function calling==========");
+// calculateSum(25, 26);
+// calculateSum();
+
+// Default Parameters
+// function calculateSum(num1 = 10, num2 = 5) {
+//   console.log(num1 + num2);
+// }
+
+// calculateSum()
+// calculateSum(25);
+// calculateSum(25,15);
+
+// Return
+
+function calculateSum(num1, num2) {
+  console.log("Calcualting sum....");
+
+  return num1 + num2;
+  return num1; // Ignore
+  return num2; // Ignore
+  console.log("Hello"); // Ignore
+}
+
+let result = calculateSum(10, 20);
+console.log(result);
