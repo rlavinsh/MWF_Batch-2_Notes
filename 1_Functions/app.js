3f291e20f4326aa4aa171a0190983547bddf94cb
rlavinsh <rlavinsh@gmail.com>
2026-06-26 19:02:12 +0530

    Functions

---
+++ 1_Functions/app.js
@@ -0,0 +1,12 @@
+// Functions
+// 1. Function Declration 
+// 2 Function expression
+// 3.Arrow Function
+//4.IIFE(Immediate Invoked function expression)
+
+//Invoke kar sakte ho
+
+// Syntax
+ function FunctionName(){
+  
+ }
---
+++ GEC/app.js
@@ -0,0 +1,18 @@
+// function c() {
+//   var d = 200;
+//   console.log("Inside function");
+//   function e(para) {
+//     console.log(para);
+//   }
+
+//   console.log(d);
+//   e("Hello JS");
+//   return "something";
+// }
+
+// var f = c();
+// console.log(f);
+// console.log("code ends");
+
+
+
