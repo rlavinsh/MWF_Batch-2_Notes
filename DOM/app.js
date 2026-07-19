e59b3508a894f164c8d78365913072564bd2b9e9
rlavinsh <rlavinsh@gmail.com>
2026-07-19 17:38:36 +0530

    code updated

--- DOM/app.js
+++ DOM/app.js
@@ -47,8 +47,116 @@
 // console.log(para);
 
 //querySelectorAll()
-let para = document.querySelectorAll("#para");
-// console.log(para);
-para.forEach((val) => {
-  console.log(val);
-});
+// let para = document.querySelectorAll("#para");
+// // console.log(para);
+// para.forEach((val) => {
+//   console.log(val);
+// });
+
+// let para = document.querySelector("p");
+// console.log(para.textContent);
+// console.log(para.innerText);
+// console.log(para.innerHTML);
+// let heading = document.querySelector("h1");
+// console.log(heading);
+// heading.textContent = "Hello world";
+// heading.innerHTML = `<u>Hello world</u>`;
+// document.body.style.backgroundColor = "yellow";
+// let body = document.body;
+// // body.innerText = "hello"
+// let heading = document.createElement("h2");
+// heading.textContent = "DOM Class";
+
+// yeah mere document mein last mein add ho raha hein
+// body.append(heading);
+// lekin mujhe first mein add karna hein
+// body.prepend(heading);
+// console.log(heading);
+
+// let parent = document.querySelector("#list")
+
+// let list = document.createElement("li")
+// list.innerText = "Papaya"
+// parent.append(list)
+
+// Example-1
+// <h1 id="heading">Welcome</h1>
+
+// Output
+
+// Welcome to JavaScript DOM
+
+// Example-2
+
+// <p>HTML</p>
+// <p>CSS</p>
+// <p>JavaScript</p>
+// <p>React</p>
+
+// Requirements
+
+// Sab paragraphs ko
+
+// green color
+// font size 24px
+let allPara = document.getElementsByTagName("p");
+console.log(allPara);
+for (let i = 0; i < allPara.length; i++) {
+  ((allPara[i].style.color = "green"), (allPara[i].style.fontSize = "24px"));
+}
+
+// Example-3
+// <div class="student">Rahul</div>
+// <div class="student">Priya</div>
+// <div class="student">Aman</div>
+
+// Requirements
+
+// Sab cards par
+
+// background : skyblue
+// padding :20px
+// margin :10px
+// border radius :10px
+
+// Example-4
+
+// <h2 class="company">ABC Pvt Ltd</h2>
+
+// Requirements
+
+// Text
+
+// Google India
+
+// kar do.
+
+// Color red
+
+// Example-5
+
+// <ul>
+// <li>Home</li>
+// <li>About</li>
+// <li>Services</li>
+// <li>Contact</li>
+// </ul>
+
+// Requirements
+// Sab list items ko
+
+// font-size :25px
+// color :purple
+
+// Example-6
+// Body me dynamically
+
+// Breaking News
+
+// heading create karo.
+
+// Uska
+
+// background black
+// text white
+// padding 20px
---
+++ DOM/prac.md
@@ -0,0 +1,248 @@
+Example-1
+
+<h1 id="heading">Welcome</h1>
+
+Output
+
+Welcome to JavaScript DOM
+
+
+id se select karo
+text change karo
+text ka color blue karo
+font size 40px karo
+
+Example-2
+
+<p>HTML</p>
+<p>CSS</p>
+<p>JavaScript</p>
+<p>React</p>
+
+Requirements
+
+Sab paragraphs ko
+
+green color
+font size 24px
+
+Example-3
+<div class="student">Rahul</div>
+<div class="student">Priya</div>
+<div class="student">Aman</div>
+
+Requirements
+
+Sab cards par
+
+background : skyblue
+padding :20px
+margin :10px
+border radius :10px
+
+
+Example-4
+
+<h2 class="company">ABC Pvt Ltd</h2>
+
+Requirements
+
+querySelector use karke
+
+Text
+
+Google India
+
+kar do.
+
+Color red
+
+Example-5
+
+<ul>
+<li>Home</li>
+<li>About</li>
+<li>Services</li>
+<li>Contact</li>
+</ul>
+
+Requirements
+
+querySelectorAll()
+
+Sab list items ko
+
+font-size :25px
+color :purple
+
+Example-6
+<body>
+
+</body>
+
+Requirements
+
+JavaScript se
+
+<h1>Hello Students</h1>
+
+create karo.
+
+Body me append karo.
+
+Example-7
+Body me dynamically
+
+Breaking News
+
+heading create karo.
+
+Uska
+
+background black
+text white
+padding 20px
+
+Example-8
+
+Add 5 Subjects
+
+JavaScript use karke
+
+Math
+Science
+English
+Computer
+History
+
+ke
+
+<li>
+
+create karo aur
+
+<ul>
+
+ke andar append karo.
+
+Example-9
+<h1 id="title">
+DOM Practice
+</h1>
+
+CSS
+
+.active{
+background:green;
+color:white;
+padding:20px;
+}
+
+Requirements
+
+JavaScript se
+
+active
+
+class add karo.
+
+Example-10
+
+<div id="box" class="active">
+Hello
+</div>
+
+Requirements
+
+JavaScript se
+
+active
+
+class remove karo.
+
+Example-11
+JavaScript se
+
+5 employees create karo
+
+Rahul
+Priya
+Aman
+Karan
+Neha
+
+Aur sabko
+
+<ul>
+
+me append karo.
+
+Example-12
+
+JavaScript se
+
+3 div banao
+
+Laptop
+Phone
+Watch
+
+Har card me
+
+padding
+border
+margin
+
+lagao.
+
+Example-13
+
+<p class="movie">KGF</p>
+<p class="movie">Pushpa</p>
+<p class="movie">Jawan</p>
+
+Requirements
+
+Sabka
+
+background yellow
+font size 30px
+
+Example-14
+JavaScript se
+
+Home
+About
+Services
+Gallery
+Contact
+
+ke
+
+<li>
+
+create karo aur navigation banao.
+
+Example-15
+
+JavaScript se
+
+Name : Rahul
+
+Course : MERN
+
+Batch : 2026
+
+ek
+
+<div>
+
+ke andar dikhao.
+
+Card ko
+
+padding
+border
+margin
+
+do.
