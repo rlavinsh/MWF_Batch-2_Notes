bb8fb586e0a9afbceae229d9538d3b30007edf59
rlavinsh <rlavinsh@gmail.com>
2026-07-27 18:39:07 +0530

    dom events code updated

---
+++ DOM_3/app.js
@@ -0,0 +1,44 @@
+// let para = document.querySelector("p");
+// let btn = document.querySelector(".btn");
+// btn.addEventListener("dblclick", () => {
+//   para.classList.toggle("para");
+//   //   console.log(para);
+//   if (para.classList.contains("para")) {
+//     btn.innerText = "Show text";
+//   } else {
+//     btn.innerText = "Hide text";
+//   }
+// });
+
+// let city = document.querySelector("#city");
+
+// let result = document.querySelector(".result");
+
+// city.addEventListener("change", () => {
+//   // console.log("city printing");
+//   result.textContent = city.value;
+//   console.dir(city);
+// });
+
+// let user = {
+//   firstName: "hello",
+//   age: 25,
+// };
+// console.log(user.age);
+
+// let theme = document.querySelector("#theme");
+
+// theme.addEventListener("change", () => {
+//   document.body.style.backgroundColor = theme.value;
+// });
+
+let input = document.querySelector("input");
+let result = document.querySelector(".result");
+
+input.addEventListener("input", () => {
+  result.textContent = input.value.length;
+  if (input.value.length == 5) {
+    alert("limit ho gayi");
+  }
+});
+// console.dir(input);
